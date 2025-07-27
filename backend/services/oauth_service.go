fd71cbf45c6f558f40e310dc997561f16a1566c6
Vanaraj10 <vanaraj1018@gmail.com>
2025-07-27 17:21:46 +0530

    Update Google OAuth redirect URL and add internet permission to AndroidManifest

--- backend/services/oauth_service.go
+++ backend/services/oauth_service.go
@@ -13,7 +13,7 @@ func InitGoogleOAuth() {
 	GoogleOAuthConfig = &oauth2.Config{
 		ClientID:     os.Getenv("GOOGLE_CLIEND_ID"),
 		ClientSecret: os.Getenv("GOOGLE_CLIEND_SECRET"),
-		RedirectURL:  "http://localhost:8080/auth/google/callback",
+		RedirectURL:  "http://gof-quiz-production-4390.up.railway.app/auth/google/callback",
 		Scopes:       []string{"https://www.googleapis.com/auth/userinfo.email", "https://www.googleapis.com/auth/userinfo.profile"},
 		Endpoint:     google.Endpoint,
 	}
